953ae8b9f41cc4b610422986c50e2a4aefc78829
busaidi <busaidi.nexus@gmail.com>
2023-09-26 20:13:25 +0400

    about.blade.php

--- resources/views/public/pages/about.blade.php
+++ resources/views/public/pages/about.blade.php
@@ -1,22 +1,28 @@
 @extends('public.layouts.app')
 @section('title', __('navbar.about'))
 @section('content')
-    <!-- About Us Content -->
-    <section class="about-content">
-        <h2>About Our Company</h2>
-        <p>Founded in 2016, Investment Corporation, a brand under the umbrella of Mazoon Aluminum, has a rich history deeply rooted in our unwavering commitment to quality and innovation. Our journey began with a clear mission: to deliver exceptional construction solutions to the market.</p>
+    <div class="container py-5">
 
-        <p>In response to the ever-growing demand for high-quality construction materials, we embarked on our path by venturing into UPVC fabrication. Our dedication to maintaining the highest standards of quality quickly earned us recognition within the industry. This initial success encouraged us to expand our offerings to include aluminum fabrication, further solidifying our reputation for excellence.</p>
+        <!-- Title -->
+        <h1>{{ __('about.title_1') }}</h1>
 
-        <p>As we continued to grow and evolve, we recognized the need for advanced aluminum systems that could cater to diverse architectural requirements. We started by developing non-thermal break aluminum systems, offering durable and versatile solutions for various projects. Our commitment to innovation drove us to invest in research and development, leading to the introduction of cutting-edge thermal break aluminum systems.</p>
+        <!-- Intro -->
+        <p class="lead">
+            {{ __('about.lead') }}
+        </p>
 
-        <p>These thermal break systems not only met the industry's highest standards but also excelled in energy efficiency and sustainability, aligning with the evolving needs of modern construction. Today, we proudly offer a comprehensive range of both non-thermal and thermal break aluminum solutions, catering to residential, commercial, and industrial projects.</p>
+        <!-- Journey Timeline -->
+        <h3>{{ __('about.title_2') }}</h3>
+        <ul class="list-group list-group-flush mb-4">
+            <li class="list-group-item">{{ __('about.stage_1') }}</li>
+            <li class="list-group-item">{{ __('about.stage_2') }}</li>
+            <li class="list-group-item">{{ __('about.stage_3') }}</li>
+            <li class="list-group-item">{{ __('about.stage_4') }}</li>
+        </ul>
 
-        <p>Our journey has always been guided by a dedication to innovation, adaptability, and customer satisfaction. We believe in shaping the future of the aluminum industry by delivering solutions that exceed expectations. Our commitment extends beyond innovation, as we've focused on designing systems that are easy to manufacture, reducing production time and increasing fabrication capacity.</p>
+        <!-- Mazoon Aluminum -->
+        <h2>{{ __('about.title_3') }}</h2>
+        <p>{{ __('about.lead_2') }}</p>
 
-        <p>As we reflect on our history, we take pride in the milestones we've achieved and the values that have propelled us forward. Customer-centricity, innovation, integrity, collaboration, quality, continuous improvement, and community engagement remain at the heart of our operations.</p>
-
-        <p>We look to the future with excitement, knowing that our dedication to quality and forward-thinking solutions will continue to set us apart in the industry. At Investment Corporation, a brand of Mazoon Aluminum, we remain committed to shaping the future of construction, one innovative solution at a time.</p>
-    </section>
-
+    </div>
 @endsection

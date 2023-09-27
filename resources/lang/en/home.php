fd381dc9320b077cd608ce7e718c92918155b971
busaidi <busaidi.nexus@gmail.com>
2023-09-27 16:27:48 +0400

    edite translate Oman Home & Building Expo

--- resources/lang/en/home.php
+++ resources/lang/en/home.php
@@ -2,6 +2,6 @@
 return [
     'title' => 'Home And Building Expo',
     'body' => 'With our unwavering commitment to excellence and customer satisfaction,
-     Mazoon Aluminum is a symbol of trust and quality. As we prepare for the upcoming Construction and Design Expo,
+     Mazoon Aluminum is a symbol of trust and quality. As we prepare for the upcoming Oman Home & Building Expo,
      we invite everyone to visit our booth to explore our innovations.',
 ];

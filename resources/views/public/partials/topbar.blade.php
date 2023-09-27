3722a396e8aae5ad26d2f22e2dc585d781caf479
busaidi <busaidi.nexus@gmail.com>
2023-09-27 21:31:41 +0400

    topbar.blade.php update

--- resources/views/public/partials/topbar.blade.php
+++ resources/views/public/partials/topbar.blade.php
@@ -1,9 +1,28 @@
-<div class="py-0 bg-primary">
-    <div class="container">
-        <div class="row justify-content-between">
-            <div class="col-auto">
-                <p class="mb-0 text-white">{{ __('carousel.topbar') }}</p>
-            </div>
-        </div>
+<div class="container-fluid" style="background-color: #999999;">
+    <div class="container d-flex justify-content-end py-1">
+        <!-- Phone -->
+        <a href="tel:0096891711170" target="_blank" class="text-white me-3 text-decoration-none" style="font-size: 0.9rem;">
+            <i class="fas fa-phone-alt"></i>
+        </a>
+
+        <!-- Email -->
+        <a href="mailto:busaidi.nexus@gmail.com" target="_blank" class="text-white me-3 text-decoration-none" style="font-size: 0.9rem;">
+            <i class="fas fa-envelope"></i>
+        </a>
+
+        <!-- WhatsApp -->
+        <a href="https://wa.me/0096891711170" target="_blank" class="text-white me-3 text-decoration-none" style="font-size: 0.9rem;">
+            <i class="fab fa-whatsapp"></i>
+        </a>
+
+        <!-- Instagram -->
+        <a href="https://www.instagram.com/mazoon_aluminum/" target="_blank" class="text-white me-3 text-decoration-none" style="font-size: 0.9rem;">
+            <i class="fab fa-instagram"></i>
+        </a>
+
+        <!-- LinkedIn -->
+        <a href="https://om.linkedin.com/company/mazoon-aluminum" target="_blank" class="text-white text-decoration-none" style="font-size: 0.9rem;">
+            <i class="fab fa-linkedin"></i>
+        </a>
     </div>
 </div>

7c4811165a337e1abd5c82f04d59611672b1cbe2
busaidi <busaidi.nexus@gmail.com>
2023-12-09 18:48:46 +0400

    adjust title

--- resources/views/public/layouts/app.blade.php
+++ resources/views/public/layouts/app.blade.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
     @include('public.partials.seo', [
         'title' => __('navbar.title') . ' | ' .  __('title'),
         'description' => $description,

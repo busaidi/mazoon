c7c16f239377835f079f1a6469d57f7a9484660d
busaidi <busaidi.nexus@gmail.com>
2023-12-09 19:30:59 +0400

    install laravel breeze, i need to check if correct or not

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,24 @@
 
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\NewsController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PublicController;
 use App\Http\Controllers\LanguageController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
 */
+
+
 
 // Redirect the base URL to the default locale
 Route::get('/', function () {
@@ -43,3 +51,19 @@ Route::prefix('{locale}')->middleware(['language'])->where(['locale' => 'en|ar']
 Route::get('/language/{lang}', [LanguageController::class, 'switch'])
     ->name('language.switch')
     ->where('lang', 'en|ar');
+
+/*Route::get('/', function () {
+    return view('welcome');
+});
+*/
+Route::get('/dashboard', function () {
+    return view('welcome');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
